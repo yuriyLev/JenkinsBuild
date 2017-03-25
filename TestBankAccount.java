679641b63745ba22f1d03ee9bb34865831d40ced
Yuriy <lightstream@ukr.net>
2017-03-26 00:59:30 +0200

    Adding Jacoco plugin

--- TestBankAccount.java
+++ TestBankAccount.java
@@ -5,4 +5,10 @@ import org.junit.Test;
 
 public class TestBankAccount {
 
+@Test
+  public void testDebitWithSufficientFunds() {
+	BankAccount account = new BankAccount(10);
+	double amount = account.debit(5);
+	assert.assertEquals(5.0, amount);
+ }
 }
